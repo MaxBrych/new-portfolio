d0e0290b2578780c37faf6aa186b461d26a57cc8
MaxBrych <max.brych03@gmail.com>
2023-06-25 11:59:10 +0200

    project card in dark mode

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import urlFor from "../../lib/urlFor";
 import category from "../../schemas/category";
 import ClientSideRoute from "./ClientSideRoute";
+import { Heading, useColorModeValue, Text, Box } from "@chakra-ui/react";
 
 type Props = {
   projects: Project[];
@@ -12,6 +13,10 @@ function ProjectList({ projects }: Props) {
   {
     /*console.log(posts.length);*/
   }
+  const bg = useColorModeValue("gray.100", "#0a0b0d");
+  const color = useColorModeValue("gray.500", "gray.500");
+  const border = useColorModeValue("gray.200", "gray.700");
+  const text = useColorModeValue("blackAlpha.900", "white");
 
   return (
     <div className="">
@@ -19,7 +24,11 @@ function ProjectList({ projects }: Props) {
         {/* Posts */}
         {projects.map((project) => (
           <ClientSideRoute key={project._id} route={project.link}>
-            <div className="flex flex-col cursor-pointer group">
+            <Box
+              border="1px solid"
+              borderColor={border}
+              className="flex flex-col cursor-pointer group"
+            >
               <div className="relative w-full h-56">
                 <Image
                   className="object-cover object-left rounded-2xl"
@@ -27,10 +36,12 @@ function ProjectList({ projects }: Props) {
                   alt={project.title}
                   fill
                 />
-                <div className="absolute bottom-0 flex justify-between invisible w-full p-5 text-white bg-black dark:text-black dark:bg-white bg-opacity-20 rounded-b-2xl">
+                <div className="absolute bottom-0 flex justify-between invisible w-full p-5 bg-opacity-20 rounded-b-2xl">
                   <div>
-                    <p className="text-lg font-bold">{project.title}</p>
-                    <p>
+                    <Heading color={text} className="text-lg font-bold">
+                      {project.title}
+                    </Heading>
+                    <Text color={color}>
                       {new Date(project._createdAt).toLocaleDateString(
                         "en-US",
                         {
@@ -39,17 +50,22 @@ function ProjectList({ projects }: Props) {
                           year: "numeric",
                         }
                       )}
-                    </p>
+                    </Text>
                   </div>
                 </div>
               </div>
-              <div className="flex-1 mt-4 text-white">
-                <h1 className="mb-1 text-xl font-semibold leading-5">
+              <div className="flex-1 mt-4 ">
+                <Heading
+                  color={text}
+                  className="mb-1 text-xl font-semibold leading-5"
+                >
                   {project.title}
-                </h1>
-                <p className="line-clamp-3">{project.description}</p>
+                </Heading>
+                <Text color={color} className="line-clamp-3">
+                  {project.description}
+                </Text>
               </div>
-            </div>
+            </Box>
           </ClientSideRoute>
         ))}
       </div>
