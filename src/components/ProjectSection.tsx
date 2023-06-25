cbf3cc20716bfa44d0fdc20859bd0aac3008c66f
MaxBrych <max.brych03@gmail.com>
2023-06-25 12:42:10 +0200

    fix

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -6,35 +6,51 @@ import urlFor from "../../lib/urlFor";
 import Image from "next/image";
 import { useProject } from "./useProjects";
 import Link from "next/link";
+import { Box, useColorModeValue, Text } from "@chakra-ui/react";
 
 type Props = {
   projects: Project[];
 };
 
+const bg = useColorModeValue("white", "whiteAlpha.200");
+const border = useColorModeValue("gray.200", "gray.700");
+const heading = useColorModeValue("gray.700", "gray.100");
+const text = useColorModeValue("gray.500", "gray.200");
+
 export default function ProjectSection({ projects }: Props) {
   return (
     <div className="py-4 pb-6 md:px-16 md:py-14">
       <div className="flex py-2">
-        <h1 className="mb-4 text-3xl font-medium">Projekte</h1>
+        <Text color={heading} className="mb-4 text-3xl font-medium">
+          Projekte
+        </Text>
       </div>
       <div className="grid grid-cols-1 gap-2 md:grid-cols-3 md:gap-y-16">
         {/* Posts */}
         {projects.map((project: any) => (
           <ClientSideRoute key={project._id} route={project.link}>
-            <div className="flex flex-col p-3 transition-all duration-150 bg-white border border-gray-300 cursor-pointer md:p-4 rounded-xl group ">
-              <div className="relative w-full h-56 border border-gray-800 rounded-lg md:h-80">
+            <Box
+              bg={bg}
+              border="1px solid"
+              borderColor={border}
+              className="flex flex-col p-3 transition-all duration-150 cursor-pointer md:p-4 rounded-xl group "
+            >
+              <div className="relative w-full h-40 border rounded-lg md:h-56 ">
                 <Image
-                  className="object-cover object-left rounded-lg"
+                  className="object-cover object-center rounded-lg"
                   src={urlFor(project.mainImage).url()}
                   alt={project.title}
                   fill
                 />
                 <div className="absolute bottom-0 flex justify-between invisible w-full p-5 bg-opacity-20 rounded-b-2xl">
                   <div>
-                    <p className="font-medium md:text-lg text-md">
+                    <Text
+                      color={heading}
+                      className="font-medium md:text-lg text-md"
+                    >
                       {project.title}
-                    </p>
-                    <p>
+                    </Text>
+                    <Text color={text}>
                       {new Date(project._createdAt).toLocaleDateString(
                         "en-US",
                         {
@@ -43,19 +59,22 @@ export default function ProjectSection({ projects }: Props) {
                           year: "numeric",
                         }
                       )}
-                    </p>
+                    </Text>
                   </div>
                 </div>
               </div>
               <div className="flex-1 mt-4 ">
-                <h1 className="mb-1 font-medium leading-5 md:text-xl text-md">
+                <Text
+                  color={heading}
+                  className="mb-1 font-medium leading-5 md:text-xl text-md"
+                >
                   {project.title}
-                </h1>
-                <p className="text-sm line-clamp-3 md:text-md">
+                </Text>
+                <Text color={text} className="text-sm line-clamp-3 md:text-md">
                   {project.description}
-                </p>
+                </Text>
               </div>
-            </div>
+            </Box>
           </ClientSideRoute>
         ))}
       </div>
